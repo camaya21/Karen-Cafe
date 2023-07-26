158ed5ea36a67c97d935c1a262878858c5da07b2
camaya21 <cindymamaya21@gmail.com>
2023-07-25 22:44:26 -0400

    Installed Tailwind css

--- karen-app/src/App.js
+++ karen-app/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <p className='text-3XL text-pink-600 font-bold'>
+          Welcome to Karen Cafe
+        </p>
       </header>
     </div>
   );
